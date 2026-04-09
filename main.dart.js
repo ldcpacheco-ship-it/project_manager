e474a89daf75c0642bf19a5c3afd9f447707ff09
ldcpacheco-ship-it <ldcpacheco@enginytec.com>
2026-04-09 22:16:13 +0000

    Deploying to gh-pages from @ ldcpacheco-ship-it/project_manager@d8cfe7aa7f3acfefe5fccebd3b4d5881c7a89e1b 🚀

--- main.dart.js
+++ main.dart.js
@@ -32665,7 +32665,7 @@ $S:2}
 A.aJN.prototype={
 $1(a){var s=A.ea().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:56}
 A.SF.prototype={
 gB(a){var s=this.a
